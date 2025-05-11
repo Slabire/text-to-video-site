49b0f351e1e1e9a61dae8e8ef9d673213fc5aa05
Slabire <alexandrabubu55@gmail.com>
2025-05-11 13:11:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from gtts import gTTS
 from moviepy.editor import *
 import os
@@ -14,8 +14,8 @@ def index():
 def generate_video():
     text = request.form['script']
     filename = str(uuid.uuid4())
-    audio_path = f'static/{filename}.mp3'
-    video_path = f'static/{filename}.mp4'
+    audio_path = os.path.join('static', f'{filename}.mp3')
+    video_path = os.path.join('static', f'{filename}.mp4')
     image_path = 'static/background.jpg'
 
     # Generate audio
@@ -28,7 +28,10 @@ def generate_video():
     video = imageclip.resize(height=720)
     video.write_videofile(video_path, fps=24)
 
-    return render_template('index.html', video_url=video_path)
+    # Use url_for to get the correct URL for the video file
+    video_url = url_for('static', filename=f'{filename}.mp4')
+
+    return render_template('index.html', video_url=video_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
